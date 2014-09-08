b8e320c69ecee1bc0ad9820fc2070a07c25c947c
Michael 'devbug' Williams <m.t.williams@live.com>
2014-09-08 15:38:09 -0700

    Add "user without implementation(s) has no flags" test.

--- test/ex_permissions/user_test.exs
+++ test/ex_permissions/user_test.exs
@@ -1,3 +1,15 @@
 defmodule ExPermissions.User.Test do
   use ExUnit.Case
+
+  defmodule UserWithoutImplementations do
+    use ExPermissions.User
+
+    defstruct foo: nil,
+              bar: nil
+  end
+
+  test "user without implemenation(s) has no flags" do
+    assert (%UserWithoutImplementations{} |> UserWithoutImplementations.is? :foo) == false
+    assert (%UserWithoutImplementations{} |> UserWithoutImplementations.not? :bar) == true
+  end
 end
