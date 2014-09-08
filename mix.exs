db6c4f065c6287035e32d51f2a3979b229571af9
Michael 'devbug' Williams <m.t.williams@live.com>
2014-09-08 15:08:49 -0700

    Customize Mixfile.

--- mix.exs
+++ mix.exs
@@ -2,29 +2,45 @@ defmodule ExPermissions.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex_permissions,
-     version: "0.0.1",
-     elixir: "~> 0.15.2-dev",
-     deps: deps]
+    [ app: :ex_permissions,
+      version: version,
+      name: "ExPermissions",
+      description: "Utilities for clearly specifying permissions and flags for users, groups, and credentials.",
+      source_url: "https://github.com/mtwilliams/ex_permissions",
+      elixir: "~> 0.15.2-dev",
+      homepage_url: "https://github.com/mtwilliams/ex_permissions",
+      package: [links: %{"Website" => "https://github.com/mtwilliams/ex_permissions",
+                         "Source" => "https://github.com/mtwilliams/ex_permissions"},
+                contributors: ["Michael Williams"],
+                licenses: ["Unlicense"]],
+      deps: deps(Mix.env),
+      docs: docs
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [ mod: { ExPermissions, [] },
+      applications: []
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
-  defp deps do
+  def version do
+    String.strip(File.read!("VERSION"))
+  end
+
+  defp deps(_) do
     []
+  end
+
+  defp docs do
+    case System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"]) do
+      {ref, 0} ->
+        [source_ref: ref,
+         readme: true,
+         main: "README"]
+      _ ->
+        [readme: true,
+         main: "README"]
+    end
   end
 end
